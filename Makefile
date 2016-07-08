a0c9699e0b132c60df76c3ed56966d59b1a50dd2
Scott Wilson <scott@swilson.id.au>
2016-07-08 04:21:54 +0000

    Stuff

---
+++ Makefile
@@ -0,0 +1,21 @@
+CC = cc
+CFLAGS = -std=c99 -Wall -g
+LDFLAGS = 
+FILES = prompt
+PLATFORM = $(shell uname)
+
+ifeq ($(findstring Linux,$(PLATFORM)),Linux)
+	LDFLAGS += -ledit -lm
+endif
+
+ifeq ($(findstring Darwin,$(PLATFORM)),Darwin)
+	LDFLAGS += -ledit -lm
+endif
+
+.PHONY: default
+default: lispy ;
+
+all: $(FILES)
+
+%: %.c mpc/mpc.c
+	$(CC) $(CFLAGS) $^ $(LDFLAGS) -o $@
--- prompt.c
+++ prompt.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
 #include <editline/readline.h>
 #ifndef __APPLE__
 /* Apple includes add_history in the main readline header */
 #include <editline/history.h>
-#endif /*__APPLE__*/
+#endif /* __APPLE__ */
+#endif /* _WIN32 */
 
 int main(int argc, char **argv) {
     /* Print version and exit information */
